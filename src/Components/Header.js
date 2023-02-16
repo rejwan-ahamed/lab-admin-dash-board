e22e8b9f32dba8ad53495779d5a3aa88404d2ace
Rejwan Ahamed <83488320+rejwan-ahamed@users.noreply.github.com>
2023-02-16 15:07:35 +0600

    test dark mode added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,31 @@
 import { Navbar } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "../Css/Main.css";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
+  // set theme
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handelTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   const buttonControl = () => {
     setOpen(!open);
   };
-  console.log(open);
+  console.log(theme);
+  // console.log(open);
   return (
     <>
       {/* desktop nav */}
@@ -16,15 +33,15 @@ const Header = () => {
         <Navbar
           fluid={true}
           rounded={true}
-          className=" xl:px-40 font-general max-w-[1560px] mx-auto"
+          className=" xl:px-40 font-general max-w-[1560px] mx-auto rounded-none duration-500 bg-white dark:bg-black  dark:text-white"
         >
           <Navbar.Brand to="/navbars">
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            <span className="self-center whitespace-nowrap text-xl font-semibold text-black dark:text-[#ebff00] dark:duration-500">
               Open Source <sup className="">beta</sup>
             </span>
           </Navbar.Brand>
           <Navbar.Toggle />
-          <Navbar.Collapse>
+          <Navbar.Collapse className="flex flex-row max-h-max items-center">
             <Link to={"/"} active={true} className="cursor-pointer">
               Dashboard
             </Link>
@@ -49,6 +66,37 @@ const Header = () => {
             <Link to={"/more"} className="cursor-pointer">
               Know more
             </Link>
+            {/* dark mode toggle */}
+            <div className="darkMode-button-group cursor-pointer">
+              <div
+                onClick={() => handelTheme()}
+                className="dark-mode bg-violet-50 p-2 rounded-full border border-violet-500 dark:border-[#ebff00] dark:text-black"
+              >
+                {theme === "dark" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path d="M10 2a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 2zM10 15a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 15zM10 7a3 3 0 100 6 3 3 0 000-6zM15.657 5.404a.75.75 0 10-1.06-1.06l-1.061 1.06a.75.75 0 001.06 1.06l1.06-1.06zM6.464 14.596a.75.75 0 10-1.06-1.06l-1.06 1.06a.75.75 0 001.06 1.06l1.06-1.06zM18 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5A.75.75 0 0118 10zM5 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5A.75.75 0 015 10zM14.596 15.657a.75.75 0 001.06-1.06l-1.06-1.061a.75.75 0 10-1.06 1.06l1.06 1.06zM5.404 6.464a.75.75 0 001.06-1.06l-1.06-1.06a.75.75 0 10-1.061 1.06l1.06 1.06z" />
+                  </svg>
+                )}
+              </div>
+            </div>
           </Navbar.Collapse>
         </Navbar>
       </div>
@@ -99,7 +147,10 @@ const Header = () => {
 
         {/* mobile nav main part */}
         {open ? (
-          <div onClick={()=>setOpen(!open)} className="z-[100] duration-500 fixed main-mobile-nav-section bg-gray-50 h-screen w-full flex flex-col gap-3  font-general font-[500] text-2xl text-left pt-10">
+          <div
+            onClick={() => setOpen(!open)}
+            className="z-[100] duration-500 fixed main-mobile-nav-section bg-gray-50 h-screen w-full flex flex-col gap-3  font-general font-[500] text-2xl text-left pt-10"
+          >
             <Link to={"/"} className="border-b pb-3 px-4">
               Home
             </Link>
