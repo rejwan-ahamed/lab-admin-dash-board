b5729416cc65e72fe308d3043edda15fc5b381b7
rejwan-ahamed <rejwanahamed85@gmail.com>
2023-09-18 22:58:45 +0600

    question filter added

--- src/Pages/AdminViewAnsList.js
+++ src/Pages/AdminViewAnsList.js
@@ -39,13 +39,20 @@ const AdminViewAnsList = () => {
   const count = parseInt((answers?.length * 100) / question?.length);
 
   const [ans, setAns] = useState();
+
+  // this part of the code will filter the ans and remove the trams and condition part
   useEffect(() => {
     fetch(domain + `/single_user_ans_list?roll=${ID.id}`)
       .then((res) => res.json())
-      .then((result) => setAns(result));
+      .then((result) =>
+        setAns(
+          result?.filter(
+            (datas) => datas.question !== "Terms and conditions"
+          )
+        )
+      );
   }, []);
-//   console.log(ans);
-//   console.log(ID);
+
   return (
     <div>
       <Header></Header>
@@ -53,7 +60,9 @@ const AdminViewAnsList = () => {
         <div className="wrapper-top flex justify-between items-center">
           <h2 className="font-[500] font-general text-left text-2xl dark:text-white">
             All question answer by :{" "}
-            <span className="text-violet-600 dark:text-[#EBFF00]">{userData?.name}</span>
+            <span className="text-violet-600 dark:text-[#EBFF00]">
+              {userData?.name}
+            </span>
           </h2>
           <div className="right-part mt-6 sm:mt-0">
             <h4 className="bg-violet-600 px-5 py-2 rounded-full font-general text-xl font-[550] max-w-max text-white dark:bg-[#ebff00] dark:text-black">
